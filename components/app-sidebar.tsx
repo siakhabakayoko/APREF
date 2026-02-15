0395b4474c5f133c0f720593b6f5234207995065
siakhabakayoko <siakha.bakayoko@gmail.com>
2026-02-15 11:15:33 +0000

    feat: Make app sidebar sticky to the top of the viewport.

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -68,7 +68,7 @@ export function AppSidebar() {
     const pathname = usePathname()
 
     return (
-        <Sidebar collapsible="none" className="border-r h-svh">
+        <Sidebar collapsible="none" className="border-r h-svh sticky top-0">
             <SidebarHeader className="flex items-center justify-center py-4">
                 <div className="font-bold text-xl text-primary flex items-center gap-2">
                     <Image
